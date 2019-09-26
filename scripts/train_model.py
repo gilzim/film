1ea317ea2d78cb900a99a2dc3d8216fe02460e6b
Unknown <norgrin123@gmail.com>
2019-09-26 11:55:07 +0300

    add a comment for clarification

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -103,7 +103,7 @@ parser.add_argument('--module_input_proj', default=1, type=int)  # Inp conv kern
 parser.add_argument('--module_kernel_size', default=3, type=int)
 parser.add_argument('--condition_method', default='bn-film', type=str,
                     choices=['block-input-film', 'block-output-film', 'bn-film', 'concat', 'conv-film', 'relu-film'])
-parser.add_argument('--final_resblock_with_cbn', default=4, type=int)
+parser.add_argument('--final_resblock_with_cbn', default=4, type=int)  # last Resblock that will have cbn and not bn
 parser.add_argument('--condition_pattern', default='', type=str)  # List of 0/1's (len = # FiLMs)
 parser.add_argument('--use_gamma', default=1, type=int)
 parser.add_argument('--use_beta', default=1, type=int)
