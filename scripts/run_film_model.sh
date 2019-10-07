84ff97e0ad4a084a5864d543d112252b7ad70c48
Unknown <norgrin123@gmail.com>
2019-10-07 13:36:17 +0300

    fix

--- scripts/run_film_model.sh
+++ scripts/run_film_model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python scaripts/run_model.py --program_generator data/film.pt  --execution_engine data/film.pt
+python scripts/run_model.py --program_generator data/film.pt  --execution_engine data/film.pt
