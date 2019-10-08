7186457e650164986ab9f68e08faf05be2c08d75
Unknown <norgrin123@gmail.com>
2019-10-08 11:00:18 +0300

    Vanilla Film DONT only used for creating checkpoint to test run_model.py

--- vr/models/filmed_net.py
+++ vr/models/filmed_net.py
@@ -165,7 +165,7 @@ class FiLMedNet(nn.Module):
         else:
             # film.size() = (64,4,256)
             # gammas.size() == betas.size() == (64,4,128)
-            film_gammas, cbn_gammas, film_betas, cbn_betas = torch.split(film[:, :, :4 * self.module_dim], self.module_dim, dim=-1)
+            film_gammas, film_betas = torch.split(film[:, :, :2 * self.module_dim], self.module_dim, dim=-1)
             if not self.use_gamma:
                 film_gammas = self.default_weight.expand_as(film_gammas)
                 cbn_gammas = self.default_weight.expand_as(cbn_gammas)
