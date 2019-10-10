bd51fa739ad9d5c40cd1dcf0a54fcc5236ccdc1a
Unknown <norgrin123@gmail.com>
2019-10-10 09:40:44 +0300

    changed script to run cbn and not film

--- scripts/run_model_cbn_layer4_batch96_drop0.sh
+++ scripts/run_model_cbn_layer4_batch96_drop0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python scripts/run_model.py \
---program_generator data/film.pt \
---execution_engine data/film.pt \
+--program_generator data/cbn_layer4_batch96_dropout0.pt \
+--execution_engine data/cbn_layer4_batch96_dropout0.pt \
 --input_question_h5 data/train_questions.h5 \
 --input_features_h5 data/train_features.h5
