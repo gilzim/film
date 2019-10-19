5a6db836d04eea0dd902c7927de91d74f0c12865
Unknown <gilzi@campus.technion.ac.il>
2019-10-19 03:16:38 +0300

    layer2 test

---
+++ scripts/train/cbn_layer2_batch96_dropout0_removeFrom2.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+checkpoint_path="data/cbn_layer2_batch96_dropout0_removeFrom2.pt"
+log_path="data/cbn_layer2_batch96_dropout0_removeFrom2.log"
+python scripts/train_model.py \
+  --checkpoint_path $checkpoint_path \
+  --model_type FiLM \
+  --num_iterations 133333 \
+  --print_verbose_every 200000 \
+  --checkpoint_every 66666 \
+  --record_loss_every 10000 \
+  --num_train_samples 350000 \
+  --num_val_samples 75000 \
+  --optimizer Adam \
+  --learning_rate 3e-4 \
+  --batch_size 96 \
+  --use_coords 1 \
+  --module_stem_batchnorm 1 \
+  --module_stem_num_layers 1 \
+  --module_batchnorm 1 \
+  --classifier_batchnorm 1 \
+  --bidirectional 0 \
+  --decoder_type linear \
+  --encoder_type gru \
+  --weight_decay 1e-5 \
+  --rnn_num_layers 1 \
+  --rnn_wordvec_dim 200 \
+  --rnn_hidden_dim 4096 \
+  --rnn_output_batchnorm 0 \
+  --classifier_downsample maxpoolfull \
+  --classifier_proj_dim 512 \
+  --classifier_fc_dims 1024 \
+  --module_input_proj 1 \
+  --module_residual 1 \
+  --module_dim 128 \
+  --module_dropout 0 \
+  --module_stem_kernel_size 3 \
+  --module_kernel_size 3 \
+  --module_batchnorm_affine 0 \
+  --module_num_layers 1 \
+  --num_modules 2 \
+  --condition_pattern 1,1 \
+  --gamma_option linear \
+  --gamma_baseline 1 \
+  --use_gamma 1 \
+  --use_beta 1 \
+  --condition_method bn-film \
+  --final_resblock_with_cbn 1 \
+  --program_generator_parameter_efficient 1 \
+  | tee $log_path
