863167b9a55ebc95395560567fd591d08976bd11
Unknown <norgrin123@gmail.com>
2019-10-20 10:59:31 +0300

    rename script

--- scripts/run_model_cbn_layer3_batch96_drop0.sh
+++ scripts/run_model_cbn_layer3_batch96_drop0.sh
@@ -2,8 +2,8 @@
 
 log_path="data/run_model.log"
 python scripts/run_model.py \
---program_generator data/cbn_layer4_batch96_dropout0.pt \
---execution_engine data/cbn_layer4_batch96_dropout0.pt \
+--program_generator data/cbn_layer3_batch96_dropout0.pt \
+--execution_engine data/cbn_layer3_batch96_dropout0.pt \
 --input_question_h5 data/train_questions.h5 \
 --input_features_h5 data/train_features.h5 \
 --num_samples 3000 \
